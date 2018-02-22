78f3806c0319a9a8fe14876c9c00945bfaa75b1b
jdpetticrew <jdpetticrew1@sheffield.ac.uk>
2018-02-22 14:33:34 +0000

    Written all functions.

--- MCP4716.c
+++ MCP4716.c
@@ -1,1 +1,48 @@
 #include "MCP4716.h"
+
+MCP4716::MCP4716(int adrs){
+  address=adrs;
+};
+
+void MCP4716::setGain(int G){
+  if(G==1) Gain=1;
+  else Gain=2;
+};
+
+void MCP4716::setVref(int Vr){
+  // 1=Vdd, 2=VrefUnbuf, 3=VrefBuff
+  Vref=Vr;
+};
+
+void MCP4716::command(){
+  Wire.write(B1); // Write Bit
+  Wire.write(B011); //Command Bit
+  if(Vref==1){
+    Wire.write(B00); //Vdd
+  }
+  else if (Vref==2){
+    Wire.write(B10); // VrefUnbuf
+  }
+  else if (Vref==3){
+    Wire.write(B11);
+  }
+
+  Wire.write(B00); //Power down Bits
+
+  if (G==1){
+    Wire.Write(B0); //Gain=1
+  }
+  else Wire.Write(B1); //Gain =2
+};
+
+void MCP4716::setVout(uint16_t Vout){
+  uint16_t set= Vout << 6;
+  byte set_upper= (byte) (set >> 8);
+  byte set_lower= (byte) (set && 0x0f);
+
+  Wire.beginTransmission(address);
+  command();
+  Wire.write(set_upper);
+  Wire.write(set_lower);
+  Wire.endTransmission;
+};
--- MCP4716.h
+++ MCP4716.h
@@ -5,16 +5,15 @@
 #include <stdint.h>
 class MCP4716{
 private:
-  command();
-  int address=0x60;
+  void command();
+  int address=0x60; //DEFAULT ADDRESS
   int Gain=1;
-  uint16_t Vout=0;
   int Vref=1;
   int Cmd=3; // Option Not Available to user, function to be added
 public:
-  MCP4716(int address);
-  setGain(int Gain); //1=x1, 2=x2
-  setVout(uint16_t Vout);
-  setVref(int Vref); // 1=Vdd, 2=VrefUnbuf, 3=VrefBuff
+  MCP4716(int adrs);
+  void setGain(int G); //1=x1, 2=x2
+  void setVout(uint16_t Vout);
+  void setVref(int Vr); // 1=Vdd, 2=VrefUnbuf, 3=VrefBuff
 };
 #endif
